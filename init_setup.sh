b1c020ce5e8f1c8b5427ad24cfa0dc167daea783
“Aariz <aariz120@gmail.com>
2024-05-07 12:24:18 +0530

    requirements added

--- init_setup.sh
+++ init_setup.sh
@@ -1,10 +1,10 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating a conda env with python 3.10"
-conda create -p ./envchainlit python=3.10 -y
-echo [$(date)]: "created conda envchainlit"
-source activate ./envchainlit
-echo [$(date)]: "activated conda envchainlit"
+conda create -p ./chainenv python=3.10 -y
+echo [$(date)]: "created conda env"
+source activate ./chainenv
+echo [$(date)]: "activated conda env"
 echo [$(date)]: "installing requirements"
-pip install -r requirements.txt
+pip install -r requirements.txt`
 echo [$(date)]: "installed all the requirement"
 echo [$(date)]: "END"
