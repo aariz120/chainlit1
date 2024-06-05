52fa5c893c4badc37ebcdd8d7028a94e9c0f47ac
“Aariz <aariz120@gmail.com>
2024-06-05 14:14:35 +0530

    set up file added

--- build/lib/src/llm.py
+++ build/lib/src/llm.py
@@ -3,16 +3,12 @@ from dotenv import load_dotenv
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-from src.config import instruction
-
 
 
 load_dotenv()
 
 GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
-
-message=[{"role": "system",  "content":instruction}]
 
 def ask_bot(message):
     llm= ChatGoogleGenerativeAI(model="gemini-pro")
