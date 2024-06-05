40044efdd4dcb785ecea0739e38e76b96f51ea5a
“Aariz <aariz120@gmail.com>
2024-06-05 12:30:30 +0530

    app.py added

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import chainlit as cl
-from src.llm import ask_bot
-from src.config import instruction
+
+
 
 @cl.on_message
-async def main(user_message:cl.Message):
-
-    response=ask_bot(user_message.content,instruction)
-    
+async def main(message:cl.Message):
+  
     # Send a response back to the user
     await cl.Message(
-        content=f"Received: {response}",
+        content=f"Received: {message.content}",
     ).send()
