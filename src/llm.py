358077fd10117438750c39b2b6c9fa76286a932b
“Aariz <aariz120@gmail.com>
2024-06-05 17:37:40 +0530

    final code added

--- src/llm.py
+++ src/llm.py
@@ -3,27 +3,33 @@ from dotenv import load_dotenv
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+from langchain_core.messages import HumanMessage, SystemMessage
+
 from src.config import instruction
-
-
 
 load_dotenv()
 
 GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
-user_message="can you tell me about the menu?"
-def ask_bot(user_message):
-    message=[{"role": "system",  "content":instruction},
-           {"role": "user", "content": user_message}]
+def ask_bot(user_message,instruction):
     
-    llm= ChatGoogleGenerativeAI(model="gemini-pro")
+    model = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
     
-    response=llm.invoke(message)
     
-    return response.content
+    respones=model(
+    [
+        SystemMessage(content=instruction),
+        HumanMessage(content=user_message),
+    ]
+)
     
+    return respones.content
 
+if __name__=="__main__":
+    user_message = "hi how are you?"
+    respones=ask_bot(user_message,instruction)
+    print(respones)
 
               
                                    
